a9c8be13f1573a0cb3ba7ed4e51c905142a52214
sya-ri <sya79lua@gmail.com>
2020-07-12 15:37:21 +0900

    テンプレートの型を変更

--- src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/template/HeaderFooterTemplate.kt
+++ src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/template/HeaderFooterTemplate.kt
@@ -3,15 +3,16 @@ package com.gitlab.nitgc.kokasai.the23rd.routes.template
 import io.ktor.html.Placeholder
 import io.ktor.html.Template
 import io.ktor.html.insert
-import kotlinx.html.FlowContent
+import kotlinx.html.BODY
+import kotlinx.html.FOOTER
+import kotlinx.html.HEADER
 import kotlinx.html.HTML
 import kotlinx.html.body
-import kotlinx.html.div
 import kotlinx.html.footer
 import kotlinx.html.header
 
 object HeaderFooterTemplate: Template<HTML> {
-    val body = Placeholder<FlowContent>()
+    val body = Placeholder<BODY>()
 
     override fun HTML.apply() {
         body {
@@ -26,18 +27,14 @@ object HeaderFooterTemplate: Template<HTML> {
     }
 }
 
-object HeaderTemplate: Template<FlowContent> {
-    override fun FlowContent.apply() {
-        div {
-            +"Header"
-        }
+object HeaderTemplate: Template<HEADER> {
+    override fun HEADER.apply() {
+        +"Header"
     }
 }
 
-object FooterTemplate: Template<FlowContent> {
-    override fun FlowContent.apply() {
-        div {
-            +"Footer"
-        }
+object FooterTemplate: Template<FOOTER> {
+    override fun FOOTER.apply() {
+        +"Footer"
     }
 }
