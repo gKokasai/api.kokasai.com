0caf3f855e7f5aacfdac3447109cbc730840ad7a
sya-ri <sya79lua@gmail.com>
2020-07-20 15:12:25 +0900

    ハンバーガーメニューの内容を追加 スタイル要素にIDを設定

--- src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/template/WithHeaderTemplate.kt
+++ src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/template/WithHeaderTemplate.kt
@@ -18,8 +18,8 @@ object WithHeaderTemplate : Template<HTML> {
                 @Suppress("DEPRECATION")
                 +"""
                     $(function(){
-                        $('#hamburger_menu').on('click', function() {
-                            $(this).toggleClass('active');
+                        $('#hamburger_menu_icon').on('click', function() {
+                            $('.hamburger_menu').toggleClass('active');
                             return false;
                         });
                     });
@@ -28,24 +28,38 @@ object WithHeaderTemplate : Template<HTML> {
         }
         body {
             header {
-                h1 {
-                    a(href = "/") {
-                        +"工華祭"
+                div {
+                    id = "inner_header"
+                    h1 {
+                        id = "header_title"
+                        a(href = "/") {
+                            +"工華祭"
+                        }
+                    }
+                    // ハンバーガーメニューアイコン
+                    div("hamburger_menu") {
+                        id = "hamburger_menu_icon"
+                        span()
+                        span()
+                        span()
+                    }
+                    // 横並びメニュー
+                    ul {
+                        id = "horizontal_menu"
+                        onTouchStart = ""
+                        menu("#", "ページ")
+                        menu("#", "ページ")
+                        menu("/account", "学内の方へ")
                     }
                 }
                 // ハンバーガーメニュー
-                div {
-                    id = "hamburger_menu"
-                    span()
-                    span()
-                    span()
-                }
-                // 横並びメニュー
-                ul {
-                    onTouchStart = ""
-                    menu("#", "ページ")
-                    menu("#", "ページ")
-                    menu("/account", "学内の方へ")
+                div("hamburger_menu") {
+                    id = "hamburger_menu_content"
+                    ul {
+                        menu("#", "ページ")
+                        menu("#", "ページ")
+                        menu("/account", "学内の方へ")
+                    }
                 }
             }
             insert(body)
@@ -53,7 +67,7 @@ object WithHeaderTemplate : Template<HTML> {
     }
 
     private fun UL.menu(href: String, name: String) {
-        li {
+        li("menu_element") {
             a(href = href) {
                 +name
             }
@@ -70,28 +84,28 @@ object WithHeaderTemplate : Template<HTML> {
         "li" {
             listStyleType = ListStyleType.none
         }
-        "header" {
+        "#inner_header" {
             display = Display.flex
             backgroundColor = THEME_COLOR
             padding(vertical = max(0.8.vh, 8.px))
             boxShadow(SHADOW_COLOR.withAlpha(0.4), offsetY = 0.16.vh, blurRadius = 0.32.vh)
         }
-        "header h1" {
+        "#header_title" {
             fontSize = max(3.2.vh, 32.px)
             margin(vertical = LinearDimension.auto)
             paddingTop = 0.32.vh
             paddingLeft = 2.24.vh
         }
-        "header h1 a" {
+        "#header_title a" {
             color = BASE_COLOR
             textDecoration = TextDecoration.none
         }
         // ハンバーガーメニューを使用
         media("(max-aspect-ratio: $useHamburgerAspectRatio)") {
-            "header ul" {
+            "#horizontal_menu" {
                 display = Display.none
             }
-            "#hamburger_menu" {
+            "#hamburger_menu_icon" {
                 position = Position.relative
                 margin(LinearDimension.auto)
                 marginRight = 2.24.vh
@@ -99,7 +113,7 @@ object WithHeaderTemplate : Template<HTML> {
                 height = 44.px
                 cursor = Cursor.pointer
             }
-            "#hamburger_menu span" {
+            "#hamburger_menu_icon span" {
                 position = Position.absolute
                 left = 0.px
                 width = 100.pct
@@ -107,61 +121,68 @@ object WithHeaderTemplate : Template<HTML> {
                 backgroundColor = BASE_COLOR
                 borderRadius = 4.px
             }
-            "#hamburger_menu, #hamburger_menu span" {
+            "#hamburger_menu_icon, #hamburger_menu_icon span" {
                 display = Display.inlineBlock
                 transition("all", 0.5.s)
                 boxSizing = BoxSizing.borderBox
             }
-            "#hamburger_menu span:nth-of-type(1)" {
+            "#hamburger_menu_icon span:nth-of-type(1)" {
                 top = 0.px
             }
-            "#hamburger_menu span:nth-of-type(2)" {
+            "#hamburger_menu_icon span:nth-of-type(2)" {
                 top = 20.px
             }
-            "#hamburger_menu span:nth-of-type(3)" {
+            "#hamburger_menu_icon span:nth-of-type(3)" {
                 bottom = 0.px
             }
-            "#hamburger_menu.active span:nth-of-type(1)" {
+            "#hamburger_menu_content" {
+                display = Display.none
+            }
+            "#hamburger_menu_icon.active span:nth-of-type(1)" {
                 transform {
                     translateY(20.px)
                     rotate(45.deg)
                 }
             }
-            "#hamburger_menu.active span:nth-of-type(2)" {
+            "#hamburger_menu_icon.active span:nth-of-type(2)" {
                 opacity = 0
             }
-            "#hamburger_menu.active span:nth-of-type(3)" {
+            "#hamburger_menu_icon.active span:nth-of-type(3)" {
                 transform {
                     translateY((-20).px)
                     rotate((-45).deg)
                 }
             }
+            "#hamburger_menu_content.active" {
+                display = Display.block
+                width = 100.pct
+            }
         }
         // 横並びメニューを使用
         media("(min-aspect-ratio: $useHamburgerAspectRatio)") {
-            "#hamburger_menu" {
+            ".hamburger_menu" {
                 display = Display.none
             }
-            "header ul" {
+            "#horizontal_menu" {
                 fontSize = max(2.24.vh, 24.px)
                 margin(LinearDimension.auto)
                 marginRight = 0.vh
                 paddingTop = 0.224.vh
             }
-            "header li" {
+            ".menu_element" {
                 padding(0.vh, 2.24.vh)
                 display = Display.inline
             }
-            "header li+ li" {
+            ".menu_element+ .menu_element" {
                 borderLeft(0.24.vh, BorderStyle.solid, BASE_COLOR)
             }
-            "header li a" {
+            ".menu_element a" {
                 fontWeight = FontWeight.w500
                 color = BASE_COLOR
                 textDecoration = TextDecoration.none
                 position = Position.relative
             }
-            "header li a::after" {
+            ".menu_element a::after" {
                 position = Position.absolute
                 bottom = (-0.16).vh
                 left = 0.vh
@@ -173,7 +194,7 @@ object WithHeaderTemplate : Template<HTML> {
                 }
                 transition("transform", .2.s)
             }
-            "header li a:hover::after" {
+            ".menu_element a:hover::after" {
                 transform {
                     scale(1, 1)
                 }
