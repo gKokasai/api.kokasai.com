a533ad30f1e2abfe1800c70d0f4313601329d4a8
sya-ri <sya79lua@gmail.com>
2020-07-29 03:55:37 +0900

    IDEに指摘された場所を修正 Reformat Codeを実行

--- src/main/resources/static/header.js
+++ src/main/resources/static/header.js
@@ -1,26 +1,27 @@
-$(function(){
-    $('#hamburger_icon').on('click', function() {
-        if($('html').hasClass('active_hamburger')){
+$(function () {
+    $('#hamburger_icon').on('click', function () {
+        let html = $('html');
+        if (html.hasClass('active_hamburger')) {
             $('#hamburger_menu').slideUp(200);
         } else {
             $('#hamburger_menu').slideDown(500);
         }
-        $('html').toggleClass('active_hamburger');
+        html.toggleClass('active_hamburger');
         return false;
     });
-    $('#hamburger_menu .menu_element a').on('click', function(e){
+    $('#hamburger_menu .menu_element a').on('click', function (e) {
         $('html').removeClass('active_hamburger');
         $('#hamburger_menu').slideUp(200);
         e.preventDefault();
-        url = $(this).attr('href');
-        if(url != ''){
-            setTimeout(function(){
+        let url = $(this).attr('href');
+        if (url !== '') {
+            setTimeout(function () {
                 window.location = url;
             }, 250);
         }
         return false;
     });
-    $('#hamburger_background').on('click', function(){
+    $('#hamburger_background').on('click', function () {
         $('html').removeClass('active_hamburger');
         $('#hamburger_menu').slideUp(200);
     });
