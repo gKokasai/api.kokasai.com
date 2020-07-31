3f9ae2e1929ec72a354e884acd145ba5eeca6b84
sya-ri <sya79lua@gmail.com>
2020-07-31 23:33:05 +0900

    再起動してもセッションが消えないように変更

--- src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/configure/Sessions.kt
+++ src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/configure/Sessions.kt
@@ -2,12 +2,13 @@ package com.gitlab.nitgc.kokasai.the23rd.configure
 
 import com.gitlab.nitgc.kokasai.the23rd.constants.CookiesConstants
 import com.gitlab.nitgc.kokasai.the23rd.user.UserPrincipal
-import io.ktor.sessions.SessionStorageMemory
 import io.ktor.sessions.Sessions
 import io.ktor.sessions.cookie
+import io.ktor.sessions.directorySessionStorage
+import java.io.File
 
 fun Sessions.Configuration.configureAuthCookie() {
-    cookie<UserPrincipal>(CookiesConstants.AUTH, storage = SessionStorageMemory()) {
+    cookie<UserPrincipal>(CookiesConstants.AUTH, storage = directorySessionStorage(File(".sessions"), true)) {
         cookie.path = "/"
     }
 }
