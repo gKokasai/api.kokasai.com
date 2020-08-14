2ae947b9d687e5a7553f590d277c1b176690bc65
sya-ri <sya79lua@gmail.com>
2020-08-14 13:36:40 +0900

    バストークンを追加

--- src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/ApiRoute.kt
+++ src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/ApiRoute.kt
@@ -1,6 +1,9 @@
 package com.gitlab.nitgc.kokasai.the23rd.routes
 
+import io.ktor.application.ApplicationCall
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.request.header
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
@@ -9,10 +12,35 @@ import io.ktor.routing.route
 fun Routing.apiRoute() {
     route("/api") {
         route("/bus") {
+            get("/challenge") {
+                BusTokenManager.challenge(call) {
+                    call.respond(HttpStatusCode.OK, "Succeed Challenge")
+                }
+            }
             get("/route") {
-                call.respond(BusRoute.route)
+                BusTokenManager.challenge(call) {
+                    call.respond(BusRoute.route)
+                }
             }
         }
+    }
+}
+
+object BusTokenManager {
+    suspend inline fun challenge(call: ApplicationCall, onSuccess: () -> Unit) {
+        if(checkValid(call)){
+            onSuccess.invoke()
+        } else {
+            call.respond(HttpStatusCode.Forbidden, "Failed Challenge")
+        }
+    }
+
+    fun checkValid(call: ApplicationCall): Boolean {
+        return checkValid(call.request.header("token"))
+    }
+
+    private fun checkValid(token: String?): Boolean {
+        return token == "abcdef" // TODO 仮のトークン確認
     }
 }
 
