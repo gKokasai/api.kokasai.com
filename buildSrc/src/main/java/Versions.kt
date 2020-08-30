d49ef0703379d92ab6a33c805d29174810f0f0e5
sya-ri <sya79lua@gmail.com>
2020-08-30 19:14:46 +0900

    kotlin-css のバージョンを更新

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val kotlin = "1.4.0"
     const val ktor = "1.4.0"
     const val shadow = "6.0.0"
-    const val kotlin_css_jvm = "1.0.0-pre.110-kotlin-${kotlin}"
+    const val kotlin_css_jvm = "1.0.0-pre.112-kotlin-${kotlin}"
     const val logback = "1.2.3"
 }
