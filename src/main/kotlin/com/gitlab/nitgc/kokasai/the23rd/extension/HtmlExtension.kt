043eab544227a14b48b9164380191bc237e6aa58
sya-ri <sya79lua@gmail.com>
2020-09-20 14:09:08 +0900

    queryParameters が空のリダイレクト時 ? だけついてしまうバグの修正

--- src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/extension/HtmlExtension.kt
+++ src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/extension/HtmlExtension.kt
@@ -16,6 +16,12 @@ fun Route.route(path: HtmlRoute.Path, build: Route.() -> Unit): Route = route(pa
 @ContextDsl
 fun Route.get(path: HtmlRoute.Path, body: PipelineInterceptor<Unit, ApplicationCall>) = get(path.path, body)
 
-suspend inline fun ApplicationCall.respondRedirect(url: HtmlRoute.Path, queryParameters: List<String> = listOf(), permanent: Boolean = false) = respondRedirect(url.full_path + queryParameters.joinToString("&", "?"), permanent)
+suspend inline fun ApplicationCall.respondRedirect(url: HtmlRoute.Path, queryParameters: List<String> = listOf(), permanent: Boolean = false) {
+    if (queryParameters.isEmpty()) {
+        respondRedirect(url.full_path, permanent)
+    } else {
+        respondRedirect(url.full_path + queryParameters.joinToString("&", "?"), permanent)
+    }
+}
 
 suspend inline fun ApplicationCall.respondRedirect(url: Pair<HtmlRoute.Path, List<String>>, permanent: Boolean = false) = respondRedirect(url.first, url.second, permanent)
