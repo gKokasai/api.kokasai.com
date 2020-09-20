c87e3637f7449f55bde9a550d90b5d5c2012a89f
sya-ri <sya79lua@gmail.com>
2020-09-20 16:32:42 +0900

    不要な関数オーバーライドを削除

--- src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/html/RouteBuilder.kt
+++ src/main/kotlin/com/gitlab/nitgc/kokasai/the23rd/routes/html/RouteBuilder.kt
@@ -4,13 +4,8 @@ import com.gitlab.nitgc.kokasai.the23rd.extension.*
 import com.gitlab.nitgc.kokasai.the23rd.routes.*
 import com.gitlab.nitgc.kokasai.the23rd.routes.html.access.*
 import com.gitlab.nitgc.kokasai.the23rd.routes.html.api.*
-import io.ktor.routing.*
 
 object HtmlRouteBuilder: RouteBuilder.Container {
-    override fun build(route: Route) {
-        super.build(route)
-    }
-
     override val routes = mapOf(
         HtmlRoute.Index to index,
         HtmlRoute.Login to login,
