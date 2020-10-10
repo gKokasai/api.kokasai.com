13d8ca5ad42d54afb7384ff8577144fb321a5a60
sya-ri <sya79lua@gmail.com>
2020-10-10 23:48:33 +0900

    ライブラリのバージョンを更新 バージョン確認ができるURLをコメントで追加

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    const val kotlin = "1.4.0"
-    const val ktor = "1.4.0"
-    const val shadow = "6.0.0"
-    const val kotlin_css_jvm = "1.0.0-pre.112-kotlin-${kotlin}"
-    const val exposed = "0.24.1"
-    const val sqlite_driver = "3.30.1"
-    const val logback = "1.2.3"
+    const val kotlin = "1.4.10" // https://search.maven.org/search?q=g:org.jetbrains.kotlin
+    const val ktor = "1.4.1" // https://bintray.com/kotlin/ktor/ktor
+    const val shadow = "6.1.0" // https://bintray.com/johnrengelman/gradle-plugins/gradle-shadow-plugin
+    const val kotlin_css_jvm = "1.0.0-pre.124-kotlin-${kotlin}" // https://bintray.com/kotlin/kotlin-js-wrappers/kotlin-css
+    const val exposed = "0.24.1" // https://bintray.com/kotlin/exposed/exposed-core
+    const val sqlite_driver = "3.32.3.2" // https://search.maven.org/artifact/org.xerial/sqlite-jdbc
+    const val logback = "1.2.3" // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
 }
