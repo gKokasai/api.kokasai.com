d6d77debb3d3d47d2c8d2d4c71716de07c75f91a
sya-ri <sya79lua@gmail.com>
2020-10-11 12:35:04 +0900

    ハンバーガーメニューのページ遷移動作を Kotlin/JS に移行

--- src/jsMain/kotlin/HeaderJs.kt
+++ src/jsMain/kotlin/HeaderJs.kt
@@ -11,10 +11,24 @@ object HeaderJs: Js {
         get() = document.getElementById(Header.Id.hamburger_icon) as? HTMLElement
     val hamburger_menu
         get() = document.getElementById(Header.Id.hamburger_menu) as? HTMLElement
+    val hamburger_menu_element_a
+        get() = document.querySelectorAll("#${Header.Id.hamburger_menu} .${Header.Class.menu_element} a").asList()
 
     override val onLoad: ((Event) -> dynamic)? = {
         hamburger_icon?.addEventListener("click", {
             body?.classList?.toggle(Header.Class.active_hamburger)
         })
+        hamburger_menu_element_a.forEach {
+            if (it !is HTMLElement) return@forEach
+            it.addEventListener("click", { event ->
+                body?.removeClass(Header.Class.active_hamburger)
+                event.preventDefault()
+                val url = it.getAttribute("href")
+                if (url.isNullOrEmpty()) return@addEventListener
+                window.setTimeout({
+                    window.location.href = url
+                }, 250)
+            })
+        }
     }
 }
--- src/jvmMain/resources/static/header.js
+++ src/jvmMain/resources/static/header.js
@@ -1,16 +1,4 @@
 $(function () {
-    $('#hamburger_menu .menu_element a').on('click', function (e) {
-        $('html').removeClass('active_hamburger');
-        $('#hamburger_menu').slideUp(200);
-        e.preventDefault();
-        let url = $(this).attr('href');
-        if (url !== '') {
-            setTimeout(function () {
-                window.location = url;
-            }, 250);
-        }
-        return false;
-    });
     $('#hamburger_background').on('click', function () {
         $('html').removeClass('active_hamburger');
         $('#hamburger_menu').slideUp(200);
