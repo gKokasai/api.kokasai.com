d3b8cef2037b8cd89ddeb22e818491d9348232d0
sya-ri <sya79lua@gmail.com>
2020-10-14 02:17:47 +0900

    OptimizeImports を実行

--- src/jvmMain/kotlin/com/gitlab/nitgc/kokasai/the23rd/Launch.kt
+++ src/jvmMain/kotlin/com/gitlab/nitgc/kokasai/the23rd/Launch.kt
@@ -1,17 +1,5 @@
 package com.gitlab.nitgc.kokasai.the23rd
 
-import com.gitlab.nitgc.kokasai.the23rd.configure.*
-import com.gitlab.nitgc.kokasai.the23rd.routes.*
-import com.gitlab.nitgc.kokasai.the23rd.routes.html.*
-import io.ktor.application.*
-import io.ktor.auth.*
-import io.ktor.features.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import io.ktor.sessions.*
-import io.ktor.websocket.*
-
 fun main() {
     Kokasai23rd.launch()
 }
