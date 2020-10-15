353704ff1863106b65cd587eda4cfaae8f19b220
sya-ri <sya79lua@gmail.com>
2020-10-15 17:32:32 +0900

    WebSocket接続の処理をJsに実装

--- src/jsMain/kotlin/BusUpdaterJs.kt
+++ src/jsMain/kotlin/BusUpdaterJs.kt
@@ -1,30 +1,31 @@
+import constants.*
+import org.w3c.dom.*
 import org.w3c.dom.events.*
 
 object BusUpdaterJs: Js {
     override val onLoad: ((Event) -> dynamic) = {
-        /*
-        let socket = new WebSocket("ws://localhost:8080/bus");
-
-        socket.onopen = function(event) {
-            alert("[open] Connection established");
-        };
-
-        socket.onmessage = function(event) {
-            alert(`[message] Data received from server: ${event.data}`);
-        };
-
-        socket.onclose = function(event) {
-            if (event.wasClean) {
-                alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-            } else {
-                // サーバのプロセスが停止 ネットワークダウン
-                alert('[close] Connection died');
+        WebSocket("$WEB_SOCKET_HOST:$WEB_SOCKET_PORT/bus").apply {
+            onopen = {
+                console.info("[open] Connection established")
             }
-        };
-
-        socket.onerror = function(event) {
-            alert(`[error] ${event.message}`);
-        };
-         */
+            onmessage = { event ->
+                console.info("[message] Data received from server: ${event.data}")
+            }
+            onclose = { event ->
+                if (event is CloseEvent) {
+                    if (event.wasClean) {
+                        console.info("[close] Connection closed cleanly, code=${event.code} reason=${event.reason}")
+                    } else {
+                        // サーバのプロセスが停止 ネットワークダウン
+                        console.info("[close] Connection died")
+                    }
+                }
+            }
+            onerror = { event ->
+                if (event is ErrorEvent) {
+                    console.info("[error] ${event.message}")
+                }
+            }
+        }
     }
 }
--- src/jsMain/kotlin/main.kt
+++ src/jsMain/kotlin/main.kt
@@ -1,3 +1,4 @@
 fun main() {
     HeaderJs.apply()
+    BusUpdaterJs.apply()
 }
