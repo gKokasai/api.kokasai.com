25be8d29d70e16a248651284c42596cd68578b44
sya-ri <sya79lua@gmail.com>
2020-11-27 22:57:52 +0900

    LocalFile,UnSupportFileProviderを追加

---
+++ src/jvmMain/kotlin/com/kokasai/flowerkt/file/UnSupportFileProvider.kt
@@ -0,0 +1,17 @@
+package com.kokasai.flowerkt.file
+
+import java.io.*
+
+object UnSupportFileProvider: FileProvider {
+    override suspend fun add(path: String, file: File): Boolean {
+        return false
+    }
+
+    override suspend fun remove(path: String): Boolean {
+        return false
+    }
+
+    override suspend fun get(path: String): File? {
+        return null
+    }
+}
