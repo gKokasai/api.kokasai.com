12f330da519432107b51e9b45868a13e7fed551f
sya-ri <sya79lua@gmail.com>
2021-01-18 14:42:39 +0900

    ライブラリのバージョンを更新

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    const val kotlin = "1.4.20" // https://search.maven.org/search?q=g:org.jetbrains.kotlin
-    const val ktor = "1.4.2" // https://bintray.com/kotlin/ktor/ktor
+    const val kotlin = "1.4.21" // https://search.maven.org/search?q=g:org.jetbrains.kotlin
+    const val ktor = "1.4.1" // https://bintray.com/kotlin/ktor/ktor
     const val shadow = "6.1.0" // https://bintray.com/johnrengelman/gradle-plugins/gradle-shadow-plugin
-    const val kotlin_css_jvm = "1.0.0-pre.129-kotlin-${kotlin}" // https://bintray.com/kotlin/kotlin-js-wrappers/kotlin-css
+    const val kotlin_css_jvm = "1.0.0-pre.138-kotlin-1.4.10" // https://bintray.com/kotlin/kotlin-js-wrappers/kotlin-css
     const val exposed = "0.28.1" // https://bintray.com/kotlin/exposed/exposed-core
-    const val sqlite_driver = "3.32.3.2" // https://search.maven.org/artifact/org.xerial/sqlite-jdbc
+    const val sqlite_driver = "3.34.0" // https://search.maven.org/artifact/org.xerial/sqlite-jdbc
     const val logback = "1.2.3" // https://mvnrepository.com/artifact/ch.qos.logback/logback-classic
 }
