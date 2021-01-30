d8de6b40d6c0d77facc0a07a6f6072e72d6595ee
sya-ri <sya79lua@gmail.com>
2021-01-30 14:26:46 +0900

    chore: FlowerKt のバージョンを更新。必要な依存関係を再導入

--- buildSrc/src/main/java/Libraries.kt
+++ buildSrc/src/main/java/Libraries.kt
@@ -13,20 +13,20 @@ object Libraries {
 
     object Ktor {
         const val client_okhttp = "io.ktor:ktor-client-okhttp:${Versions.ktor}"
+        const val server_auth = "io.ktor:ktor-auth:${Versions.ktor}"
         const val server_netty = "io.ktor:ktor-server-netty:${Versions.ktor}"
         const val server_sessions = "io.ktor:ktor-server-sessions:${Versions.ktor}"
         const val gson = "io.ktor:ktor-gson:${Versions.ktor}"
+        const val websocket = "io.ktor:ktor-websockets:${Versions.ktor}"
     }
 
     object FlowerKt {
         const val repositoryUrl = "https://dl.bintray.com/kokasai/maven"
-        const val auth = "com.kokasai.flowerkt:auth:${Versions.flowerkt}"
         const val core = "com.kokasai.flowerkt:core:${Versions.flowerkt}"
         const val css = "com.kokasai.flowerkt:css:${Versions.flowerkt}"
         const val database_sqlite = "com.kokasai.flowerkt:database-exposed-sqlite:${Versions.flowerkt}"
         const val file_webdav = "com.kokasai.flowerkt:file-webdav:${Versions.flowerkt}"
         const val session_exposed = "com.kokasai.flowerkt:session-exposed:${Versions.flowerkt}"
-        const val websocket = "com.kokasai.flowerkt:websocket:${Versions.flowerkt}"
     }
 
     const val logback = "ch.qos.logback:logback-classic:${Versions.logback}"
