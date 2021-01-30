e2e6d82af10e67a809b4ccfee9da0ac43f42f410
sya-ri <sya79lua@gmail.com>
2021-01-30 15:08:10 +0900

    fix: / にアクセスした際に 404 Error が出る問題を修正

---
+++ src/main/kotlin/com/kokasai/the23rd/routes/http/index.kt
@@ -0,0 +1,13 @@
+package com.kokasai.the23rd.routes.http
+
+import com.kokasai.flowerkt.route.RouteAction
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+
+val index: RouteAction = {
+    get {
+        call.respond(HttpStatusCode.OK)
+    }
+}
