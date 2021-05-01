53a9b77e8b68d8ecb373cbaba1a4dd8c4248db53
sya-ri <sya79lua@gmail.com>
2021-05-02 01:31:57 +0900

    feat: /group/form/list を追加

--- src/main/kotlin/com/kokasai/api/http/HttpRoute.kt
+++ src/main/kotlin/com/kokasai/api/http/HttpRoute.kt
@@ -3,6 +3,7 @@ package com.kokasai.api.http
 import com.kokasai.flowerkt.route.RoutePath
 import com.kokasai.api.http.group.document.list as groupDocumentList
 import com.kokasai.api.http.group.form.get as groupFormGet
+import com.kokasai.api.http.group.form.list as groupFormList
 import com.kokasai.api.http.group.form.submit as groupFormSubmit
 import com.kokasai.api.http.group.user.list as groupUserList
 import com.kokasai.api.http.user.document.list as userDocumentList
@@ -23,6 +24,7 @@ object HttpRoute : RoutePath("/") {
         object Form : RoutePath(this, "/form") {
             override val child = setOf(Get, Submit)
 
+            object List : RoutePath(this, "/list", action = groupFormList)
             object Get : RoutePath(this, "/get", action = groupFormGet)
             object Submit : RoutePath(this, "/submit", action = groupFormSubmit)
         }
---
+++ src/main/kotlin/com/kokasai/api/http/group/form/list.kt
@@ -0,0 +1,40 @@
+package com.kokasai.api.http.group.form
+
+import com.kokasai.api.auth.UserLogin
+import com.kokasai.api.group.Group
+import com.kokasai.api.user.User
+import com.kokasai.flowerkt.route.RouteAction
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.sessions.get
+import io.ktor.sessions.sessions
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ListResponse(val form: List<String>)
+
+val list: RouteAction = {
+    get("{groupName}") {
+        val principal = call.sessions.get<UserLogin.Data>()
+        if (principal != null) {
+            val groupName = call.parameters["groupName"]
+            if (groupName != null) {
+                val userName = principal.name
+                val user = User.get(userName)
+                val groups = user.file.group
+                if (groups.contains(groupName)) {
+                    val group = Group.get(groupName)
+                    call.respond(ListResponse(group.file.form))
+                } else {
+                    call.respond(HttpStatusCode.Forbidden)
+                }
+            } else {
+                call.respond(HttpStatusCode.BadRequest)
+            }
+        } else {
+            call.respond(HttpStatusCode.Unauthorized)
+        }
+    }
+}
