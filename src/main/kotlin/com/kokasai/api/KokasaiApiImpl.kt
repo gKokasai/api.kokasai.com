38fa45915f81877b9ba4842cf6b2bf5087b56310
sya-ri <sya79lua@gmail.com>
2021-05-08 22:54:43 +0900

    perf: ktor-client を OkHttp から CIO に変更

--- src/main/kotlin/com/kokasai/api/KokasaiApiImpl.kt
+++ src/main/kotlin/com/kokasai/api/KokasaiApiImpl.kt
@@ -16,7 +16,7 @@ import com.kokasai.flowerkt.module.UseMailSendGrid
 import io.ktor.application.Application
 import io.ktor.application.install
 import io.ktor.auth.Authentication
-import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.engine.cio.CIO
 import io.ktor.features.AutoHeadResponse
 import io.ktor.features.CORS
 import io.ktor.features.CallLogging
@@ -31,7 +31,7 @@ class KokasaiApiImpl : KokasaiApi, UseFileWebDav, UseMailSendGrid {
     override val logger: Logger = LoggerFactory.getLogger("KokasaiAPI")
     override val engine = Netty
     override val port = SystemEnv.Server.Port ?: 8080
-    override val fileProvider = WebDAVFileProvider(OkHttp, SystemEnv.WebDAV.UserName, SystemEnv.WebDAV.Password, SystemEnv.WebDAV.Url)
+    override val fileProvider = WebDAVFileProvider(CIO, SystemEnv.WebDAV.UserName, SystemEnv.WebDAV.Password, SystemEnv.WebDAV.Url)
     override val databaseProvider = RemoteSQLiteDatabaseProvider(SystemEnv.Server.DatabaseFileName ?: "data.db", fileProvider, 60 * 1000)
     override val mailProvider = SendGridMailProvider(SystemEnv.SendGrid.ApiKey, "noreply@kokasai.com")
     override val routePath = setOf(HttpRoute)
