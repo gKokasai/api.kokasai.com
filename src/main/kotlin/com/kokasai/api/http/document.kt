80baa54e6018a1b670c9c10b0cfa735499e39faa
sya-ri <sya79lua@gmail.com>
2021-05-13 11:54:20 +0900

    feat: /document を削除し /group/document/list/admin で全てのドキュメントを返すようにした

--- src/main/kotlin/com/kokasai/api/http/document.kt
+++ src/main/kotlin/com/kokasai/api/http/document.kt
@@ -1,8 +1,8 @@
 package com.kokasai.api.http
 
 import com.kokasai.api.KokasaiApi.Companion.api
-import com.kokasai.api.http._dsl.onlyAdmin
 import com.kokasai.api.http._dsl.onlyUser
+import com.kokasai.api.http._dsl.parameter
 import com.kokasai.api.util.Directory
 import com.kokasai.flowerkt.route.RouteAction
 import io.ktor.application.call
@@ -10,30 +10,22 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.response.respondFile
 import io.ktor.routing.get
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class GetDocumentListResponse(val document: List<String>)
 
 val document: RouteAction = {
-    get {
-        onlyAdmin {
-            call.respond(GetDocumentListResponse(api.fileProvider.list(Directory.document).orEmpty()))
-        }
-    }
-    get("{name}") {
+    get("{documentName}") {
         onlyUser { user ->
-            val documentName = call.parameters["name"]!!
-            val accessibleDocument = user.file.getDocument()
-            if (accessibleDocument.contains(documentName)) {
-                val file = api.fileProvider.get("${Directory.document}/$documentName")
-                if (file != null) {
-                    call.respondFile(file)
+            parameter("documentName") { documentName ->
+                val accessibleDocument = user.file.getDocument()
+                if (accessibleDocument.contains(documentName)) {
+                    val file = api.fileProvider.get("${Directory.document}/$documentName")
+                    if (file != null) {
+                        call.respondFile(file)
+                    } else {
+                        call.respond(HttpStatusCode.NotFound)
+                    }
                 } else {
                     call.respond(HttpStatusCode.NotFound)
                 }
-            } else {
-                call.respond(HttpStatusCode.NotFound)
             }
         }
     }
--- src/main/kotlin/com/kokasai/api/http/group/document/list.kt
+++ src/main/kotlin/com/kokasai/api/http/group/document/list.kt
@@ -1,9 +1,11 @@
 package com.kokasai.api.http.group.document
 
+import com.kokasai.api.KokasaiApi.Companion.api
 import com.kokasai.api.group.Group
 import com.kokasai.api.http._dsl.inGroupFromParameter
 import com.kokasai.api.http._dsl.onlyAdmin
 import com.kokasai.api.http._dsl.parameter
+import com.kokasai.api.util.Directory
 import com.kokasai.flowerkt.route.RouteAction
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
@@ -22,18 +24,26 @@ data class PostListRequest(val document: List<String>)
 val list: RouteAction = {
     get("{groupName}") {
         inGroupFromParameter("groupName") { _, groupName ->
-            val group = Group.get(groupName)
-            call.respond(GetListResponse(group.file.document))
+            val document = if (groupName != Group.Name.admin) {
+                Group.get(groupName).file.document
+            } else {
+                api.fileProvider.list(Directory.document).orEmpty()
+            }
+            call.respond(GetListResponse(document))
         }
     }
     post("{groupName}") {
         parameter("groupName") { groupName ->
             onlyAdmin {
-                val group = Group.get(groupName)
-                val request = call.receive<PostListRequest>()
-                group.file.document = request.document
-                group.save()
-                call.respond(HttpStatusCode.OK)
+                if (groupName != Group.Name.admin) {
+                    val request = call.receive<PostListRequest>()
+                    Group.get(groupName).apply {
+                        file.document = request.document
+                    }.save()
+                    call.respond(HttpStatusCode.OK)
+                } else {
+                    call.respond(HttpStatusCode.Forbidden)
+                }
             }
         }
     }
