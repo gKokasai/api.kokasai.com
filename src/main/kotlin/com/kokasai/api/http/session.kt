2706238227e584040711efd0765c36622e8faf6a
sya-ri <sya79lua@gmail.com>
2021-05-20 14:43:34 +0900

    feat: /session を追加 close #15

---
+++ src/main/kotlin/com/kokasai/api/http/session.kt
@@ -0,0 +1,20 @@
+package com.kokasai.api.http
+
+import com.kokasai.api.auth.UserLogin
+import com.kokasai.api.http._dsl.onlyUser
+import com.kokasai.flowerkt.route.RouteAction
+import io.ktor.application.call
+import io.ktor.response.respond
+import io.ktor.routing.get
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class GetResponse(val count: Long)
+
+val session: RouteAction = {
+    get {
+        onlyUser { user ->
+            call.respond(GetResponse(UserLogin.getSessionCount(user.name)))
+        }
+    }
+}
