1d5b5ceff8f4248435c0f924f121a3b40860f409
sya-ri <sya79lua@gmail.com>
2021-07-05 11:08:25 +0900

    feat: onlyAdminOrGroupUser を追加

---
+++ src/main/kotlin/com/kokasai/api/http/_dsl/onlyAdminOrGroupOwner.kt
@@ -0,0 +1,32 @@
+package com.kokasai.api.http._dsl
+
+import com.kokasai.api.group.Group
+import com.kokasai.api.user.User
+import com.kokasai.api.user.User.Companion.isAdmin
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.util.pipeline.PipelineContext
+
+/**
+ * Admin かグループのオーナーであれば処理を実行する
+ * @param groupName グループ名
+ * @param onSuccess 実行する処理
+ */
+suspend inline fun PipelineContext<Unit, ApplicationCall>.onlyAdminOrGroupOwner(
+    groupName: String,
+    onSuccess: (
+        user: User,
+        group: Group
+    ) -> Unit
+) {
+    onlyUser { user ->
+        val group = Group.get(groupName)
+        if (group.file.owner.contains(user.name) || user.isAdmin) {
+            onSuccess(user, group)
+        } else {
+            call.respond(HttpStatusCode.Forbidden)
+        }
+    }
+}
--- src/main/kotlin/com/kokasai/api/http/_dsl/onlyAdminOrGroupUser.kt
+++ src/main/kotlin/com/kokasai/api/http/_dsl/onlyAdminOrGroupUser.kt
@@ -1,6 +1,5 @@
 package com.kokasai.api.http._dsl
 
-import com.kokasai.api.group.Group
 import com.kokasai.api.user.User
 import com.kokasai.api.user.User.Companion.isAdmin
 import io.ktor.application.ApplicationCall
@@ -9,18 +8,20 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.util.pipeline.PipelineContext
 
-suspend inline fun PipelineContext<Unit, ApplicationCall>.onlyAdminOrOwner(
+/**
+ * Admin かグループに属するユーザーであれば処理を実行する
+ * @param groupName グループ名
+ * @param onSuccess 実行する処理
+ */
+suspend inline fun PipelineContext<Unit, ApplicationCall>.onlyAdminOrGroupUser(
     groupName: String,
     onSuccess: (
-        user: User,
-        group: Group
+        user: User
     ) -> Unit
 ) {
     onlyUser { user ->
-        val group = Group.get(groupName)
-        val owners = group.file.owner
-        if (owners.contains(user.name) || user.isAdmin) {
-            onSuccess(user, group)
+        if (user.file.group.contains(groupName) || user.isAdmin) {
+            onSuccess(user)
         } else {
             call.respond(HttpStatusCode.Forbidden)
         }
